40acfb01a8ced9a790c1ca2bbb88754c97d2b30a
stevema <bochenma@cs.uw.edu>
2020-08-11 16:54:04 -0700

    init

---
+++ src/main/java/esDB/AmazonESSetUp.java
@@ -0,0 +1,20 @@
+package esDB;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.http.AWSRequestSigningApacheInterceptor;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AmazonESSetUp {
+
+}
