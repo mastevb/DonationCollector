4aa1cefb0f6be919bc5a7b74d5f6da258c9d0229
stevema <bochenma@cs.uw.edu>
2020-08-11 17:41:07 -0700

    edited dependnecies

--- src/main/java/esDB/AmazonESSetUp.java
+++ src/main/java/esDB/AmazonESSetUp.java
@@ -17,4 +17,36 @@ import java.util.Map;
 
 public class AmazonESSetUp {
 
+    private static String serviceName = "es";
+    private static String region = "us-east-2";
+    private static String aesEndpoint = "https://search-donationcollector-7tn3xplzj34wls3iio7j5aeapi.us-east-2.es.amazonaws.com";
+    private static String index = "my-index";
+    private static String type = "_doc";
+    private static String id = "1";
+
+    static final AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient esClient = esClient(serviceName, region);
+
+        // Create the document as a hash map
+        Map<String, Object> document = new HashMap<>();
+        document.put("title", "Walk the Line");
+        document.put("director", "James Mangold");
+        document.put("year", "2005");
+
+        // Form the indexing request, send it, and print the response
+        IndexRequest request = new IndexRequest(index, type, id).source(document);
+        IndexResponse response = esClient.index(request, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+    }
+
+    // Adds the interceptor to the ES REST client
+    public static RestHighLevelClient esClient(String serviceName, String region) {
+        AWS4Signer signer = new AWS4Signer();
+        signer.setServiceName(serviceName);
+        signer.setRegionName(region);
+        HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(serviceName, signer, credentialsProvider);
+        return new RestHighLevelClient(RestClient.builder(HttpHost.create(aesEndpoint)).setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor)));
+    }
 }
