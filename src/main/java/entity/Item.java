ba62ba842d1f674803c503a0afbc84e95d2268d8
Fishfishz <yyu18@uw.edu>
2020-08-11 22:07:43 -0700

    created item index

---
+++ src/main/java/entity/Item.java
@@ -0,0 +1,150 @@
+package entity;
+
+import java.util.Date;
+import java.util.Set;
+
+import org.elasticsearch.common.geo.GeoPoint;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Item {
+    private String name;
+    private String residentID;
+    private String description;
+    private String imageUrl;
+    private String address;
+    private GeoPoint location;
+
+    private String NGOID;
+    private String scheduleID;
+    private Date scheduleTime;
+    private int status;  // 0 for pending, 1 for scheduled, 2 for picked-up
+
+    public String getName() {
+        return name;
+    }
+
+    public String getResidentID() {
+        return residentID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public String getNGOID() {
+        return NGOID;
+    }
+
+    public String getScheduleID() {
+        return scheduleID;
+    }
+
+    public Date getScheduleTime() {
+        return scheduleTime;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    private Item(ItemBuilder builder) {
+        this.name = builder.name;
+        this.residentID = builder.residentID;
+        this.description = builder.description;
+        this.imageUrl = builder.imageUrl;
+        this.address = builder.address;
+        this.location = builder.location;
+        this.NGOID = builder.NGOID;
+        this.scheduleID = builder.scheduleID;
+        this.scheduleTime = builder.scheduleTime;
+        this.status = builder.status;
+    }
+
+
+    public JSONObject toJSONObject() {
+        JSONObject obj = new JSONObject();
+        obj.put("name", name);
+        obj.put("residentID", residentID);
+        obj.put("description", description);
+        obj.put("imageUrl", imageUrl);
+        obj.put("address", address);
+        obj.put("location", location);
+        obj.put("NGOID", NGOID);
+        obj.put("scheduleTime", scheduleTime);
+        obj.put("scheduleID", scheduleID);
+        obj.put("item status", status);
+        return obj;
+    }
+
+    public static class ItemBuilder {
+        private String name;
+        private String residentID;
+        private String description;
+        private String imageUrl;
+        private String address;
+        private GeoPoint location;
+
+        private String NGOID;
+        private String scheduleID;
+        private Date scheduleTime;
+        private int status;  // 0 for pending, 1 for scheduled, 2 for picked-up
+
+
+        public Item build() {
+            return new Item(this);
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setResidentID(String residentID) {
+            this.residentID = residentID;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public void setLocation(GeoPoint location) {
+            this.location = location;
+        }
+
+        public void setNGOID(String NGOID) {
+            this.NGOID = NGOID;
+        }
+
+        public void setScheduleID(String scheduleID) {
+            this.scheduleID = scheduleID;
+        }
+
+        public void setScheduleTime(Date scheduleTime) {
+            this.scheduleTime = scheduleTime;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+    }
+}
--- src/main/java/esDB/AmazonESSetUp.java
+++ src/main/java/esDB/AmazonESSetUp.java
@@ -1,5 +1,6 @@
 package esDB;
 
+import entity.Item;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequestInterceptor;
 import org.elasticsearch.action.index.IndexRequest;
@@ -11,7 +12,13 @@ import com.amazonaws.auth.AWS4Signer;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.http.AWSRequestSigningApacheInterceptor;
+import org.elasticsearch.common.geo.GeoPoint;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +27,7 @@ public class AmazonESSetUp {
     private static String serviceName = "es";
     private static String region = "us-east-2";
     private static String aesEndpoint = "https://search-donationcollector-7tn3xplzj34wls3iio7j5aeapi.us-east-2.es.amazonaws.com";
-    private static String index = "my-index";
+    private static String index = "item";
     private static String type = "_doc";
     private static String id = "1";
 
@@ -29,14 +36,24 @@ public class AmazonESSetUp {
     public static void main(String[] args) throws IOException {
         RestHighLevelClient esClient = esClient(serviceName, region);
 
-        // Create the document as a hash map
-        Map<String, Object> document = new HashMap<>();
-        document.put("title", "Walk the Line");
-        document.put("director", "James Mangold");
-        document.put("year", "2005");
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+        builder.startObject();
+        {
+            builder.field("name", "string");
+            builder.field("residentID", "string");
+            builder.field("description", "string");
+            builder.field("imageUrl", "string");
+            builder.field("address", "string");
+            builder.field("location", new GeoPoint());
+            builder.field("NGOID", "string");
+            builder.field("scheduleID", "string");
+            builder.timeField("scheduleTime", new Date());
+            builder.field("status", 0);
+        }
+        builder.endObject();
 
         // Form the indexing request, send it, and print the response
-        IndexRequest request = new IndexRequest(index, type, id).source(document);
+        IndexRequest request = new IndexRequest(index, type, id).source(builder);
         IndexResponse response = esClient.index(request, RequestOptions.DEFAULT);
         System.out.println(response.toString());
     }
